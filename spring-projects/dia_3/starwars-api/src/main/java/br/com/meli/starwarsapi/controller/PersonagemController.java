e6b2c32e3b5600d8f7a61e4b21fd2e669550d0ba
Arthur Massaini <arthur.massaini@mercadolivre.com>
2021-07-02 16:23:20 -0300

    Modulo 6 - Dia 3 - TT - Exercicio StarWars-Api - Acrescentando Status nas responses

--- spring-projects/dia_3/starwars-api/src/main/java/br/com/meli/starwarsapi/controller/PersonagemController.java
+++ spring-projects/dia_3/starwars-api/src/main/java/br/com/meli/starwarsapi/controller/PersonagemController.java
@@ -3,11 +3,10 @@ package br.com.meli.starwarsapi.controller;
 import br.com.meli.starwarsapi.entity.Personagem;
 import br.com.meli.starwarsapi.repository.PersonagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.annotation.HttpConstraint;
 import java.util.*;
 
 @RestController
@@ -18,12 +17,14 @@ public class PersonagemController {
     private PersonagemRepository personagemRepository;
 
     @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
     public List<Personagem> getAllCharacters() {
         List<Personagem> personagens = personagemRepository.getAll();
         return personagens;
     }
 
     @GetMapping("/{name}")
+    @ResponseStatus(HttpStatus.OK)
     public List<Personagem> getCharactersByName(@PathVariable String name) {
         List<Personagem> personagens = personagemRepository.getByName(name);
         return personagens;
